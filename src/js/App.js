4a01ad2b95fc2d5ceda2566a50d660b0d7c07888
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-09-18 10:54:14 +0300

    Task Buttons component added

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import entry from '../../entry'
+import TaskButtons from './components/TaskButtons'
+
 
 function App() {
-    const title="title from app.js UPDATED1"
+    const title = "title from app.js UPDATED1"
     return (
-        <div>
-            <h1>{title}</h1>
+        <div className="container">
+
+            <div className="row">
+                <h3 className="text-center mt-2">Download Manager</h3>
+            </div>
+            <div className="card">
+                <TaskButtons />
+                <div className="card m-3">
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">An item</li>
+                        <li className="list-group-item">A second item</li>
+                        <li className="list-group-item">A third item</li>
+                    </ul>
+                </div>
+            </div>
+
+
         </div>
     )
 }
---
+++ src/js/components/TaskButtons.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TaskButtons() {
+    return (
+        <div className="m-2 text-center">
+            <li className="btn btn-secondary m-1">Pasuse All</li>
+            <li className="btn btn-secondary m-1">Resume All</li>
+            <li className="btn btn-secondary m-1">Remove Task</li>
+            <li className="btn btn-secondary m-1">Clear History</li>
+        </div>
+    )
+}
+
+export default TaskButtons
