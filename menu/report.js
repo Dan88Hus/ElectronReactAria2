b6791aa9ca4d4566f0d810d1a070351537c795d9
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-09-21 14:18:26 +0300

    fixed reporting length

--- menu/report.js
+++ menu/report.js
@@ -15,19 +15,19 @@ if(localSItems){
         console.log(i, obUriSitems[i])
         let newRow = tbody.insertRow(i);
         // gid, id, path, progress, status
-        for (let j = 0; j < objectLength; j++){
+        for (let j = 0; j < 1; j++){
             let newCell = newRow.insertCell(j);
-            let newText = document.createTextNode(`${obUriSitems[i].status}`)
+            let newText = obUriSitems[i].status ? document.createTextNode(`${obUriSitems[i].status}`) : document.createTextNode('waiting')
             newCell.appendChild(newText)
         }
-        for (let j = 1; j < objectLength+1; j++){
+        for (let j = 1; j < 2; j++){
             let newCell = newRow.insertCell(j);
-            let newText = document.createTextNode(`${obUriSitems[i].progress}`)
+            let newText = obUriSitems[i].progress ? document.createTextNode(`${obUriSitems[i].progress}`) : document.createTextNode('0')
             newCell.appendChild(newText)
         }
-        for (let j = 2; j < objectLength+2; j++){
+        for (let j = 2; j < 3; j++){
             let newCell = newRow.insertCell(j);
-            let newText = document.createTextNode(`${obUriSitems[i].path}`)
+            let newText = obUriSitems[i].path ? document.createTextNode(`${obUriSitems[i].path}`) : document.createTextNode('waiting')
             newCell.appendChild(newText)
         }
     }
