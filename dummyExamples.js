155c6602dde290587efaadd19f9c793d756f476a
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-09-21 18:22:57 +0300

    commit

---
+++ dummyExamples.js
@@ -0,0 +1,9 @@
+// addUri methods ####################################################################
+
+// var data = '{"id":"100000111","jsonrpc":"2.0","method": "aria2.addUri", "params": [["https://file-examples-com.github.io/uploads/2017/10/file-example_PDF_1MB.pdf"]]}';
+// https://file-examples-com.github.io/uploads/2017/10/file-example_PDF_1MB.pdf
+// https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4
+
+
+// dummy "id":"316298713","gid":"79550700e04afeaa"
+// addUri ends ###############################################################################
